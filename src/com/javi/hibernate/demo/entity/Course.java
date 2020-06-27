575bc8427c3a7703aba57253c2320fab8183ec48
javierpb275 <62296694+javierpb275@users.noreply.github.com>
2020-06-27 17:57:45 +0200

    defined course relationship

--- src/com/javi/hibernate/demo/entity/Course.java
+++ src/com/javi/hibernate/demo/entity/Course.java
@@ -1,18 +1,31 @@
 package com.javi.hibernate.demo.entity;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="course")
 public class Course {
 	
-	// define our fields
+	// define our fields and annotate fields
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
 	private int id;
 	
+	@Column(name="title")
 	private String title;
 	
+	@ManyToOne(cascade= {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH})
+	@JoinColumn(name="instructor_id")
 	private Instructor instructor;
 	
 	// define constructors
@@ -61,7 +74,7 @@ public class Course {
 	}
 	
 	
-	// annotate fields
+	
 	
 	
 
